ecc3453a52b9898b403f8c368df68fcb91fc8751
Kate <eolhovaa5@gmail.com>
2023-11-04 14:17:23 +0100

    HW 2.4.1

--- HW233.js
+++ HW233.js
@@ -1,4 +1,4 @@
-let userBalance, deliveryCost, smsCode, dbDelivery, boxPost;
+/*let userBalance, deliveryCost, smsCode, dbDelivery, boxPost;
 userBalance = 500;
 deliveryCost = 100;
 smsCode = "A001DFX0";
@@ -21,4 +21,43 @@ if (smsCode in dbDelivery) {
   }
 } else {
   console.log("Посылка с указанным кодом не найдена в базе данных.");
-}
+}*/
+
+let expencesExamples = [
+  {
+    yearlyExpences: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpences: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpences: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+let value = 0;
+let yearlyExpences = expencesExamples[value].yearlyExpences;
+
+/*let sum1=yearlyExpences.reduce (function(sum,element){
+  if (element>1000){
+   return sum + element;
+   } else {
+    return sum;
+   }
+});
+ console.log (sum1);*/
+
+expencesExamples.forEach(function (expensesExample, index) {
+  let sum = 0;
+  expensesExample.yearlyExpences.forEach(function (expense) {
+    if (expense > 1000) {
+      sum += expense;
+    }
+  });
+  console.log(`Сумма затрат выше 1000 для массива ${index + 1}: ${sum}`);
+});
