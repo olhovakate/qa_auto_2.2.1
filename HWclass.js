63c80ee0b4ff270ab6a495d4811bc28220224b57
Kate <eolhovaa5@gmail.com>
2023-11-09 14:10:15 +0100

    changed HW 2.3.2

--- HWclass.js
+++ HWclass.js
@@ -16,7 +16,7 @@ function testButton() {
 
 testButton();*/
 
-class Button {
+/*class Button {
   constructor(width, height) {
     this.props = {
       type: "button",
@@ -36,4 +36,36 @@ function testButton() {
   button.onClick();
 }
 
-testButton();
+testButton();*/
+
+class Button {
+  constructor(color = "green", type = "button", width = 100, height = 50) {
+    this.color = color;
+    this.type = type;
+    this.width = width;
+    this.height = height;
+  }
+  onClick() {
+    const exampleButton = `Color:${this.color},Type:${this.type},Width:${this.width},Height:${this.height}`;
+    console.log(exampleButton);
+  }
+}
+function testButton(anyButton) {
+  let defaultButton = new Button();
+
+  if (
+    anyButton.color === defaultButton.color &&
+    anyButton.type === defaultButton.type &&
+    anyButton.width === defaultButton.width &&
+    anyButton.height === defaultButton.height
+  ) {
+    console.log("test passed");
+  } else {
+    console.log("test failed");
+  }
+}
+let button1 = new Button("green", "button", 100, 50);
+let button2 = new Button("blue", "submit", 120, 60);
+
+testButton(button1);
+testButton(button2);
